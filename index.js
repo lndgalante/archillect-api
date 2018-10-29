defcfaecee71642b8e92ca71fe35e6fa2a5ecef5
lndgalante <lndgalante@gmail.com>
2018-10-29 01:39:39 -0300

    Add cache

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const { send } = require('micro')
 const cors = require('micro-cors')()
 const { router, get } = require('microrouter')
+const cache = require('micro-cacheable')
 const scrapper = require('./scrapper')
 
-const visuals = async (req, res) => {
+const visuals = cache(24 * 60 * 60 * 1000, async (req, res) => {
   try {
     send(res, 200, await scrapper.visualsRoute(req.query.per))
   } catch (error) {
     send(res, 500, 'Internal server error')
   }
-}
+})
 
-const random = async (req, res) => {
+const random = cache(24 * 60 * 60 * 1000, async (req, res) => {
   try {
     send(res, 200, await scrapper.randomRoute())
   } catch (error) {
     send(res, 500, 'Internal server error')
   }
-}
+})
 
-const id = async (req, res) => {
+const id = cache(24 * 60 * 60 * 1000, async (req, res) => {
   try {
     send(res, 200, await scrapper.idRoute(req.params.id))
   } catch (error) {
     send(res, 500, 'Internal server error')
   }
-}
+})
 
-const notFound = (req, res) => {
+const notFound = cache(24 * 60 * 60 * 1000, (req, res) => {
   send(res, 404, {
     error: 'Not found route',
     docs: 'https://github.com/lndgalante/archillect-unoffcial-api',
   })
-}
+})
 
 module.exports = router(
   get('/visuals', cors(visuals)),
