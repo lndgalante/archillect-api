e1e26366b40f04cb8888ed68047fba623acd3dbc
pacocoursey <pvco.coursey@gmail.com>
2018-11-16 19:38:55 -0700

    add custom cache, remove micro-cacheable dep, fix caching of routes

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 const { send } = require('micro')
 const cors = require('micro-cors')()
 const { router, get } = require('microrouter')
-const cache = require('micro-cacheable')
 const ms = require('ms')
 const scrapper = require('./scrapper')
+const cache = require('./cache');
 
-const visuals = cache(ms('10m'), async (req, res) => {
-  try {
-    send(res, 200, await scrapper.visualsRoute(req.query.per))
-  } catch (error) {
-    send(res, 500, 'Internal server error')
-  }
-})
+const visuals = async (req, res) => {
+  const data = await cache(ms('10m'), async (req) => {
+    try {
+      return await scrapper.visualsRoute(req.query.per)
+    } catch (error) {
+      return null
+    }
+  }, req ,res)
 
-const random = cache(ms('10m'), async (req, res) => {
-  try {
-    send(res, 200, await scrapper.randomRoute())
-  } catch (error) {
-    send(res, 500, 'Internal server error')
+  if (!data) {
+    send(res, 500, 'Internal Server Error')
+  } else {
+    send(res, 200, data)
   }
-})
+}
 
-const id = cache(ms('24h'), async (req, res) => {
-  try {
-    send(res, 200, await scrapper.idRoute(req.params.id))
-  } catch (error) {
-    send(res, 500, 'Internal server error')
+const random = async (req, res) => {
+  const data = await cache(ms('10m'), async () => {
+    try {
+      return await scrapper.randomRoute()
+    } catch (error) {
+      return null
+    }
+  }, req, res)
+
+  if (!data) {
+    send(res, 500, 'Internal Server Error')
+  } else {
+    send(res, 200, data)
   }
-})
+}
+
+const id = async (req, res) => {
+  const data = await cache(ms('24h'), async (req) => {
+    try {
+      return await scrapper.idRoute(req.params.id)
+    } catch (error) {
+      return null
+    }
+  }, req, res)
+
+  if (!data) {
+    send(res, 500, 'Internal Server Error')
+  } else {
+    send(res, 200, data)
+  }
+}
 
 const notFound = (req, res) => {
   send(res, 404, {
